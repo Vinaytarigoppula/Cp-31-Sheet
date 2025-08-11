23b2cdada9ada717882b41ce2d323145182bcca9
Vinaytarigoppula <vinaytarigoppula@gmail.com>
2025-08-11 19:46:34 +0530

    Extremely Round

---
+++ 800/p30.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+public class p30 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        sc.nextLine();
+        int[] dp=new int[999999+1];
+        int oper=0;
+        for(int i=1;i<=999999;i++)
+        {
+            if(helper(i))
+            {
+                oper++;
+            }
+            dp[i]=oper;
+        }
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            System.out.println(dp[n]);        
+        }
+    }
+    public static boolean helper(int i)
+    {
+        int notzero=0;
+        while(i!=0)
+        {
+            int k=i%10;
+            if(k!=0) notzero++;
+            if(notzero>1) return false;
+            i/=10;
+        }
+        return true;
+    }
+}
